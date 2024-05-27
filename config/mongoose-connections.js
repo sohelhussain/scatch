6241bc00b5c2c3f395f86d43b5e75fcc7e756850
sohelhussain <sohelhussaing@gmail.com>
2024-05-27 17:48:34 +0530

    wip:-router, models

---
+++ config/mongoose-connections.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose
+.connect('mongodb://127.0.0.1:27017/scatch')
+.then(()=> console.log("connect"))
+.catch(err => console.log(err))
+
+module.exports = mongoose.Connection;
---
+++ models/product-model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+    image: String,
+    name: String,
+    price: String,
+    discount:{
+        types: Number,
+        default: 0
+    },
+    bgcolor: String,
+    panelcolor: String,
+    textcolor: String
+});
+
+module.exports = mongoose.model('Product', productSchema);
